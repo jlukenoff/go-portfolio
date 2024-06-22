7cdde8be085bb851dd1d010387941bb5314270f4
John Lukenoff <johnlukenoff@asana.com>
2024-06-22 16:27:12 -0700

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jlukenoff/go-portfolio
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	tpl, err := template.ParseFiles("client/index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tpl.Execute(w, nil)
+}
+
+func main() {
+	http.HandleFunc("/", index)
+
+	fs := http.FileServer(http.Dir("client"))
+	http.Handle("/client/", http.StripPrefix("/client/", fs))
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Printf("Server running on port %s\n", port)
+	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+}
